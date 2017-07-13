c04931fd20c252f7c2c79a1add6b9598f595af1f
Mohammad Ashfaq <flik2009@gmail.com>
2017-07-14 01:18:30 +0500

    Update ApiController.php

--- laravel/app/Http/Controllers/ApiController.php
+++ laravel/app/Http/Controllers/ApiController.php
@@ -13,12 +13,14 @@ use Response;
 use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller {
-
-   
-
-    public function respond($data, $code=200, $headers = []) {
-	header('Content-Type: application/json');
-        return Response::json($data, $code, $headers);
-    }
+	/**
+	* respond function for json response.
+	* @param array $data
+	* @return json
+	*/
+	public function respond($data, $code=200, $headers = []) {
+		header('Content-Type: application/json');
+		return Response::json($data, $code, $headers);
+	}
  
 }
